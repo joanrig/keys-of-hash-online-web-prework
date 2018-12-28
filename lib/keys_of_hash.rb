2422d542ecc49142899af5d1403bb24bb94ea8a7
Joan in DC <joanrigdon@gmail.com>
2018-12-28 02:19:12 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require "pry"
+
+  # let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+    
+  # it "returns an array" do
+  #   expect(animals.keys_of('Panama').class).to eq(Array)
+  # end
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array_o_keys = []
+    self.each do |key, value|
+      if arguments.include?(value)
+        array_o_keys.push(key)
+      end
+    end
+    array_o_keys
   end
+  #binding.pry
 end
+ 
